876faa29c30d248d77581aaf420f87a9f1b3c306
Hassaan Raza <hassaan_raza1@yahoo.com>
2025-02-17 23:06:11 +0400

    updated portfolio

--- js/main.js
+++ js/main.js
@@ -118,15 +118,15 @@
 	/*----------------------------------------------------*/
 	/*	Modal Popup
 	------------------------------------------------------*/
-   $('.item-wrap a').magnificPopup({
+//    $('.item-wrap a').magnificPopup({
 
-      type:'inline',
-      fixedContentPos: false,
-      removalDelay: 300,
-      showCloseBtn: false,
-      mainClass: 'mfp-fade'
+//       type:'inline',
+//       fixedContentPos: false,
+//       removalDelay: 300,
+//       showCloseBtn: false,
+//       mainClass: 'mfp-fade'
 
-   });
+//    });
 
    $(document).on('click', '.popup-modal-dismiss', function (e) {
    	e.preventDefault();
